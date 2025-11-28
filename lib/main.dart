58d4c0aedbf197a11011b95cdf712dba5f49c369
imPCamuriDev <pedrogabrielofc@hotmail.com>
2025-11-28 12:46:38 -0300

    websockets working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const LoginPage(), // Começa na tela de login
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
